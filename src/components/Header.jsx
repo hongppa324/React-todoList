6679374602382e7c8c343d50639039d604c1b687
Peter Hong <t2023-m0052@Peter-Hong-MacBook-Air.local>
2024-01-23 11:23:50 +0900

    [MOD] Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="header">
       <span>
-        <strong>Channie's Todo List</strong>
+        <strong>My Todo List</strong>
       </span>
       <span>React 4th 2023, nbcamp</span>
     </header>
